86f0da57ebc1e1e2c5d38c95383a3167a5f87df6
marcodem11 <marcodemicheleta01@outlook.it>
2022-02-07 19:50:31 +0100

    allert

---
+++ app.js
@@ -0,0 +1,9 @@
+//console.log('Hello World!')
+
+let kmViaggio = parseFloat(prompt ('Quanti km devi percorrere?'));
+let età = parseInt (prompt('Quanti anni hai?'));
+console.log(kmViaggio, età);
+
+const COSTO_KM = 0.21;
+const SCONTO_GIOVANI = 0.2;
+const SCONTO_ANZIANI = 0.4;
