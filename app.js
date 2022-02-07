120fda3a39edbdab2a5759d0e0725af3ff0daeef
marcodem11 <marcodemicheleta01@outlook.it>
2022-02-07 19:55:31 +0100

    if & else

--- app.js
+++ app.js
@@ -7,3 +7,33 @@ console.log(kmViaggio, età);
 const COSTO_KM = 0.21;
 const SCONTO_GIOVANI = 0.2;
 const SCONTO_ANZIANI = 0.4;
+
+if( isNaN(kmViaggio) & isNaN(età) ) {
+
+    console.error (' I dati inseriti sono sbagliati')
+
+} else {
+
+    console.log('Dati corretti')
+    
+    let prezzoBase = kmViaggio * COSTO_KM;
+    console.log(prezzoBase);
+
+    let sconto = 0;
+}
+
+    if(età < 18) {
+        //giovani
+        let sconto = prezzoBase * SCONTO_GIOVANI;
+        console.log(sconto);
+
+
+    } else if(età > 65){
+       //anziani 
+       let sconto = prezzoBase * SCONTO_ANZIANI;
+       console.log(sconto);
+    }
+
+    // sconto
+    //let sconto = prezzoBase * SCONTO_GIOVANI
+    //let prezzo = prezzoBase - sconto
